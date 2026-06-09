15c23bbc20a8e91a0232da93931e30fd8cfaf2d1
LilianaAdornato <LilianaAdornato@github.com>
2026-06-09 16:13:14 +0800

    qqgame

--- wordmonitor_qq.go
+++ wordmonitor_qq.go
@@ -13,7 +13,6 @@ import (
 	"fmt"
 	"github.com/bitly/go-simplejson"
 	"github.com/go-resty/resty/v2"
-	"log"
 	"net/url"
 	"sort"
 	"strings"
@@ -120,16 +119,13 @@ func (m *_qqWanMonitor) check(req *_qqWanMonitorReq) (result Ret, err error) {
 		SetBody(body).
 		Post(_qqApiUrl)
 	if err != nil {
-		log.Printf("err:%v", err)
 		return
 	}
 
 	retJson, err := simplejson.NewJson(response.Body())
 	if err != nil {
-		log.Printf("err:%v", err)
 		return
 	}
-	log.Printf("qq params %+v, resp: %+v", params, retJson)
 	ret := retJson.Get("ret").MustInt(-1)
 	if ret == 0 {
 		textResultList := retJson.Get("text_result_list_")
@@ -153,7 +149,6 @@ func (m *_qqWanMonitor) check(req *_qqWanMonitorReq) (result Ret, err error) {
 }
 
 func (m *_qqWanMonitor) CheckChat(data *CommonData) (Ret, error) {
-	log.Printf("CheckChat:%+v", data)
 	var platformUniquePlayerId = GetPlatformUid(data.PlatformUniquePlayerId)
 
 	ret, err := m.check(&_qqWanMonitorReq{
@@ -166,7 +161,6 @@ func (m *_qqWanMonitor) CheckChat(data *CommonData) (Ret, error) {
 }
 
 func (m *_qqWanMonitor) CheckName(data *CommonData) (Ret, error) {
-	log.Printf("CheckName:%+v", data)
 	var platformUniquePlayerId = GetPlatformUid(data.PlatformUniquePlayerId)
 
 	ret, err := m.check(&_qqWanMonitorReq{
